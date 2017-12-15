653b5305fab0b6f18b35e4df6b405d8cb7491574
Mahdi Kashani Pour <mahdi.kashanipour@gmail.com>
2017-12-15 14:51:40 -0500

    if-else works well ! before putting it in a def

--- functional_code.py
+++ functional_code.py
@@ -30,37 +30,9 @@ pertussis_observed_state = res.observed
 pertussis_seasonal_state = res.seasonal
 pertussis_trend_state = res.trend
 
-import numpy as np
-from sklearn.linear_model import LinearRegression
-import matplotlib.pyplot as plt
-from linear_fit import linear_fit #local function
-
 #============================================ Segmented Linear Regression ==============================================
 from segmented_linear_regression import piecewise
 #=======================================================================================================================
-#to do piecewise on Trend
-trend_t = np.array(Time_Series[:len(pertussis_trend_state.dropna())])
-trend_y = np.array(pertussis_trend_state.dropna())
-
-piecewise_fited_trend = piecewise(trend_t, trend_y)
-
-#to do piecewise on Observed
-observed_t = np.array(Time_Series[:len(pertussis_observed_state.dropna())])
-observed_y = np.array(pertussis_observed_state.dropna())
-
-piecewise_fited_observed = piecewise(observed_t, observed_y)
-
-#to do piecewise on LOG observed
-log_observed_t = np.array(Time_Series[:len(pertussis_observed_state.dropna())]) #observed_t
-log_observed_y = np.log(observed_y+0.01)
-
-piecewise_fited_log_observed = piecewise(log_observed_t,log_observed_y)
-
-#to do Piecewise on LOG trend
-log_trend_t = trend_t
-log_trend_y = np.log(trend_y)
-
-piecewise_fited_log_trend = piecewise(log_trend_t,log_trend_y)
 
 import ews
 import ews_plot
@@ -68,7 +40,14 @@ import ews_plot
 name = input("(t/trend o/observe lt/logtrend lo/logobserve) please enter the name : ")
 
 if (name=='t'):
-#pdf for trend
+
+    #to do piecewise on Trend
+    trend_t = np.array(Time_Series[:len(pertussis_trend_state.dropna())])
+    trend_y = np.array(pertussis_trend_state.dropna())
+
+    piecewise_fited_trend = piecewise(trend_t, trend_y)
+
+    #pdf for trend
     filename="Cal_using_mean/"+str(state_name)+"_Trend"+".pdf"
 
     ews_df = ews.get_ews(trend_y, windowsize=100, ac_lag=1)
@@ -80,6 +59,13 @@ if (name=='t'):
     ews_plot.ews_plot (ews_df,signals,filename,"trend")
 
 elif (name=='lt'):
+
+    # to do Piecewise on LOG trend
+    log_trend_t = np.array(Time_Series[:len(pertussis_trend_state.dropna())])  # trend_t
+    log_trend_y = np.log(np.array(pertussis_trend_state.dropna()))  # trend_y = np.array()
+
+    piecewise_fited_log_trend = piecewise(log_trend_t, log_trend_y)
+
     #pdf for log trend
     filename="Cal_using_mean/"+str(state_name)+"_LogTrend"+".pdf"
 
@@ -92,6 +78,13 @@ elif (name=='lt'):
     ews_plot.ews_plot (ews_df,signals,filename,"Log_trend")
 
 elif (name=='o'):
+
+    # to do piecewise on Observed
+    observed_t = np.array(Time_Series[:len(pertussis_observed_state.dropna())])
+    observed_y = np.array(pertussis_observed_state.dropna())
+
+    piecewise_fited_observed = piecewise(observed_t, observed_y)
+
     #pdf for observed
     filename="Cal_using_mean/"+str(state_name)+"_observed"+".pdf"
 
@@ -104,8 +97,15 @@ elif (name=='o'):
     ews_plot.ews_plot (ews_df,signals,filename,"observed")
 
 elif(name=="lo"):
-#pdf for log observed
-    filename="Cal_using_mean/"+str(state_name)+"log_Trend"+".pdf"
+
+    #to do piecewise on LOG observed
+    log_observed_t = np.array(Time_Series[:len(pertussis_observed_state.dropna())])
+    log_observed_y = np.log(np.array(pertussis_observed_state.dropna())+0.01) #observed_y = np.array---.dropna()
+
+    piecewise_fited_log_observed = piecewise(log_observed_t,log_observed_y)
+
+    #pdf for log observed
+    filename="Cal_using_mean/"+str(state_name)+"log_Observed"+".pdf"
 
     ews_df = ews.get_ews(log_observed_y, windowsize=100, ac_lag=1)
     ews_df["Time"] = log_observed_t
@@ -114,5 +114,6 @@ elif(name=="lo"):
     ews_df["piecewise_fited"] = piecewise_fited_log_observed #Added Now
 
     ews_plot.ews_plot (ews_df,signals,filename,"log_observed")
+
 else:
     print("your input for name is not defined. you should use lt/lo/t/o instead.")
